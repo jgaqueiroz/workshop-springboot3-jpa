225840758d7a227b16ad139801df994751b90261
José Queiroz <jqueiroz.av@gmail.com>
2025-06-21 22:45:09 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/effecta/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.effecta.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.effecta.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
